fe8bce0be6cdbcda68492047f4897c9360bf23e6
rumon5h <mdrumonkhan48519@gmail.com>
2024-02-03 15:43:08 +0600

    flash light

---
+++ app/src/main/java/com/tecjets/flashlight/MainActivity.java
@@ -0,0 +1,104 @@
+package com.tecjets.flashlight;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleFlashLightOnOff;
+    private CameraManager cameraManager;
+    private String getCameraID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Register the ToggleButton with specific ID
+        toggleFlashLightOnOff = findViewById(R.id.toggle_flashlight);
+
+        // cameraManager to interact with camera devices
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        // Exception is handled, because to check whether
+        // the camera resource is being used by another
+        // service or not.
+        try {
+            // O means back camera unit,
+            // 1 means front camera unit
+            getCameraID = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // RequiresApi is set because, the devices which are
+    // below API level 10 don't have the flash unit with
+    // camera.
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void toggleFlashLight(View view) {
+        if (toggleFlashLightOnOff.isChecked()) {
+            // Exception is handled, because to check
+            // whether the camera resource is being used by
+            // another service or not.
+            try {
+                // true sets the torch in ON mode
+                cameraManager.setTorchMode(getCameraID, true);
+
+                // Inform the user about the flashlight
+                // status using Toast message
+                Toast.makeText(MainActivity.this, "Flashlight is turned ON", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+                // prints stack trace on standard error
+                // output error stream
+                e.printStackTrace();
+            }
+        } else {
+            // Exception is handled, because to check
+            // whether the camera resource is being used by
+            // another service or not.
+            try {
+                // true sets the torch in OFF mode
+                cameraManager.setTorchMode(getCameraID, false);
+
+                // Inform the user about the flashlight
+                // status using Toast message
+                Toast.makeText(MainActivity.this, "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+                // prints stack trace on standard error
+                // output error stream
+                e.printStackTrace();
+            }
+        }
+    }
+    // when you click on button and torch open and
+    // you do not close the torch again this code
+    // will off the torch automatically
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void finish() {
+        super.finish();
+        try {
+            // true sets the torch in OFF mode
+            cameraManager.setTorchMode(getCameraID, false);
+
+            // Inform the user about the flashlight
+            // status using Toast message
+            Toast.makeText(MainActivity.this, "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
+        } catch (CameraAccessException e) {
+            // prints stack trace on standard error
+            // output error stream
+            e.printStackTrace();
+        }
+    }
+}
